ea8fc4eb7e5f4fc0145f7418409f8a5d038a405e
MICAAP <81267700+MICAAP@users.noreply.github.com>
2021-03-28 21:53:17 -0300

    js update

---
+++ assets/js/script.js
@@ -0,0 +1,2 @@
+window.alert(' Você esta acessando meu portifolio, criado e atualizado por min.')
+window.alert(' Qualquer critica ou comentario e bem vindo, este deve ser feito por E-mail. Obrigado pela atenção')
